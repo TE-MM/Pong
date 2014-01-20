e8792f3e50c5cce6a527ef6b2ac5f0a570c13840
evinstk <tanner.evins@gmail.com>
2014-01-20 16:27:58 -0600

    0.0.1.201401201628: Slightly modified license statement

--- include/TEMM/Entities/ResourceManager.hpp
+++ include/TEMM/Entities/ResourceManager.hpp
@@ -1,11 +1,12 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Entities - A Library of Things, not Pixels
+// Part of the TEMM libraries
 // Copyright (c) 2014 Tanner Evins and Matthew Miller
 // Permission to use, copy, modify, distribute and sell this software for any
 //     purpose is hereby granted without fee, provided that the above copyright
 //     notice appear in all copies and that both that copyright notice and this
 //     permission notice appear in supporting documentation.
-// The author makes no representations about the suitability of this software
+// The authors make no representations about the suitability of this software
 //     for any purpose. It is provided "as is" without express or implied
 //     warranty.
 ////////////////////////////////////////////////////////////////////////////////
